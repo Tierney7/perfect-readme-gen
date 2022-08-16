a89ec2ac934281a9cc2bc14d751b07e3194d2531
tierney7 <tierneybbq@gmail.com>
2022-08-15 20:41:11 -0500

    questions

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generate = require('./generateMarkdown')
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
 {
-    type: "",
-    name: "",
-    message: "",
-    
-}
+    type: "input",
+    name: "title",
+    message: "Project name?"
+ },
+ {
+    type: "input",
+    name: "description",
+    message: "Describe project"
+ },
+ {
+    type: "input",
+    name: "installation",
+    message: "Installation process"
+ },
+    {
+        type: "list",
+    name: "usage",
+    message: "Project use"
+    },
 
-]
+    {
+        type: "input",
+        name: "Credits",
+        message: "List collaberators, and or links to third party assets"
+      },
+
+{
+    type: "list",
+    name: "license",
+    message: "Select License",
+    choices: [
+        "MIT",
+        "Apache",
+        "GNU",
+        "Academic",
+        "Open",
+        "ISC"
+    ]
+  },
+
+  ]
